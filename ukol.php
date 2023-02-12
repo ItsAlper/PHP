8f6a8abe3bc6f9a83933f282863af2e94c3acc20
Alper <75456808+ItsAlper@users.noreply.github.com>
2023-02-12 21:18:13 +0100

    .

--- ukol.php
+++ ukol.php
@@ -1,43 +1,13 @@
 <?php
-
-// Funkce, která očekává int a vrací bool
-function isPositiveInt(int $number): bool {
-  if ($number > 0) {
-    return true;
-  }
-  return false;
+function addNumbers($a, $b) {
+  return $a + $b;
 }
 
-// Implicitní přetypování float na int
-$floatValue = 3.14;
-$result = isPositiveInt($floatValue);
+$num1 = "5";
+$num2 = 10;
 
-// $result bude false, protože při implicitním přetypování
-// se ztratí část desetinných míst a $floatValue se stane 3
+//Implicitní přetypování "5" na int
+$result = addNumbers($num1, $num2);
 
-// Explicitní konverze
-$explicitInt = (int) $floatValue;
-$result = isPositiveInt($explicitInt);
-
-// $result bude true, protože $explicitInt bude 3
-
-// Funkce, která očekává bool a vrací string
-function boolToString(bool $value): string {
-  if ($value) {
-    return "True";
-  }
-  return "False";
-}
-
-// Implicitní přetypování int na bool
-$intValue = 0;
-$result = boolToString($intValue);
-
-// $result bude "False", protože při implicitním přetypování
-// bude $intValue přetypován na False
-
-// Explicitní konverze
-$explicitBool = (bool) $intValue;
-$result = boolToString($explicitBool);
-
-// $result bude "False", protože $explicitBool bude False
+echo "Result: " . $result . "\n"; 
+// výstup: Result: 15
